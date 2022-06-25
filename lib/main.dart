5500fccae1cfb47404c41fcfd7196fabcca623be
Denzel Gatugu <denzelgatugu@outlook.com>
2022-06-25 16:17:09 +0300

    analytics and crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +20,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform, name: "dev");
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   setUpLocator();
   runApp(MyApp(
     firebaseAuth: FirebaseAuth.instance,
